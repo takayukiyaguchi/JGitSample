3b5f6cd58adb6f0e3489aa29646e6f68231d14d9
takayukiyaguchi <takayuki@yaguchitakayuki-no-MacBook-Pro.local>
2013-08-16 19:09:49 +0900

    add

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 import java.io.File;
+import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
 
 public class Main {
 	private static String localURL = "/Users/takayuki/works/src/JGitSample-github";
 	private static String remoteURL = "https://github.com/takayukiyaguchi/JGitSample.git";
 
 	public static void main(String[] args) {
-		try {
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		test02();
 	}
 
 	private static void test01() {
 		try {
 			Git.cloneRepository().setURI(remoteURL).setDirectory(new File(localURL)).call();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test02() {
+		try {
+			Repository repo = new FileRepository(localURL + ".git");
+			Git git = new Git(repo);
+			git.pull();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
